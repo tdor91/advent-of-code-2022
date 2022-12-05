d464c852587406b9f49de33a5f5ecd6a4bcf34aa
tdor <thomas_dorfer@gmx.net>
2022-12-05 18:37:11 +0100

    Add solution for day 1

---
+++ Aoc.Day01/Aoc.D1/Program.cs
@@ -0,0 +1,26 @@
+﻿var lines = await File.ReadAllLinesAsync("input.txt");
+
+var calsPerElf = Split(lines).ToList();
+var maxCalsPerElf = calsPerElf.Select(f => f.Sum()).Max();
+
+Console.WriteLine($"Part 1: Highest calories are {maxCalsPerElf}.");
+
+var calsOfTopThree = calsPerElf.Select(f => f.Sum()).OrderByDescending(x => x).Take(3).Sum();
+Console.WriteLine($"Part 2: Combines calories of the top 3 elves are {calsOfTopThree}.");
+
+IEnumerable<IList<int>> Split(IEnumerable<string> lines)
+{
+    var result = new List<int>();
+    foreach (var line in lines)
+    {
+        if (int.TryParse(line, out var number))
+        {
+            result.Add(number);
+        }
+        else
+        {
+            yield return result;
+            result = new List<int>();
+        }
+    }
+}
